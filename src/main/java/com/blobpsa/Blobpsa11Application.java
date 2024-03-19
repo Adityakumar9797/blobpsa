309f65bc7c6313c543a9dfbd3fbd165747908935
aditya <adityakumarsharma7979@gmail.com>
2024-03-19 22:25:12 +0530

    127: create save post feature

---
+++ src/main/java/com/blobpsa/Blobpsa11Application.java
@@ -0,0 +1,13 @@
+package com.blobpsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blobpsa11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blobpsa11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/blobpsa/Blobpsa11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blobpsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Blobpsa11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
